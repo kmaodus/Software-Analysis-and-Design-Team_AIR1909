8765adc162e66ddced46b1cae2832b70521f9914
Kristijan Maoduš <kmaodus@foi.hr>
2020-02-01 17:38:37 +0100

    Implementiran ViewHolder uzorak. Dodan layout file za prikaz trgovina. Potrebno doraditi.

--- app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
+++ app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.row_main.view.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,21 +19,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listView = findViewById<ListView>(R.id.list_view_Stores)
-        listView.adapter = MyCustomAdapter(this)
+        listView.adapter = MyCustomAdapter()
     }
 
-    class MyCustomAdapter(context: Context) : BaseAdapter() {
-        private val mContext: Context = context
+    class MyCustomAdapter() : BaseAdapter() {
+        //private val mContext: Context = context
         private val storeNames = arrayListOf<String>("Varteks", "Super Nova")
 
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup): View {
-            val layoutInflater = LayoutInflater.from(mContext)
-            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+            val rowMain: View
 
-            val storesTextView = rowMain.findViewById<TextView>(R.id.stores_textview)
-            storesTextView.text = storeNames.get(position)
-            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
-            positionTextView.text = "Row number: $position"
+            if (convertView == null) {
+                val layoutInflater = LayoutInflater.from(viewGroup.context)
+                rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+
+                //val storesTextView = rowMain.findViewById<TextView>(R.id.stores_textview)
+                //val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+                val viewHolder = ViewHolder(rowMain.stores_textview, rowMain.position_textview)
+                rowMain.tag = viewHolder
+            } else {
+                rowMain = convertView
+            }
+            val viewHolder = rowMain.tag as ViewHolder
+            viewHolder.nameTextView.text = storeNames.get(position)
+            viewHolder.positionTextView.text = "Row number: $position"
             return rowMain
         }
 
@@ -46,5 +57,8 @@ class MainActivity : AppCompatActivity() {
         override fun getCount(): Int {
             return storeNames.size
         }
+
+        private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView)
+
     }
 }
