3cb631992195eec6b218d9e67dfd105e8786cf9b
Kristijan Maoduš <kmaodus@foi.hr>
2020-02-01 15:21:00 +0100

    Priprema za implementaciju View Holder uzorka. Dodan arrayListOf za popis trgovina, kao i Layout Inflater za rowMain.xml

--- app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
+++ app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.air.fumic.maodus.persic.poljak.discountlocator
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.ListView
 import android.widget.TextView
-import java.security.AccessControlContext
-import android.widget.ListView as ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,45 +18,21 @@ class MainActivity : AppCompatActivity() {
 
         val listView = findViewById<ListView>(R.id.list_view_Stores)
         listView.adapter = MyCustomAdapter(this)
-
-        /*val storesList = Recipe.getRecipesFromFile("recipes.json", this)
-        val listItems = arrayOfNulls<String>(storesList.size)
-        for (i in 0 until recipeList.size) {
-            val recipe = recipeList[i]
-            listItems[i] = recipe.title
-        }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-        listView.adapter = adapter*/
-
-        /*fun generateFakeValues(): List<String> {
-           val data = ArrayList<String>()
-           for (i in 0..4) {
-               data.add("Trgovina broj $i")
-           }
-           return data
-       }
-
-       private class CustomAdapter(context: Context, private var values:List<String>):
-           ArrayAdapter<String>(context, R.layout.list_stores_view) {
-
-           override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-               val rowView = LayoutInflater.from(context).inflate(R.layout.list_stores_view, parent, false)
-               val textView: TextView = rowView.findViewById(R.id.txtViewListStores)
-               textView.text = values[position]
-               return rowView
-           }
-       }*/
-
     }
 
     class MyCustomAdapter(context: Context) : BaseAdapter() {
-
         private val mContext: Context = context
+        private val storeNames = arrayListOf<String>("Varteks", "Super Nova")
 
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup): View {
-            val textView = TextView(mContext)
-            textView.text = "Ovo je jedan red u listi"
-            return textView
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+
+            val storesTextView = rowMain.findViewById<TextView>(R.id.stores_textview)
+            storesTextView.text = storeNames.get(position)
+            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+            positionTextView.text = "Row number: $position"
+            return rowMain
         }
 
         override fun getItem(position: Int): Any {
@@ -71,8 +44,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getCount(): Int {
-            return 8
+            return storeNames.size
         }
     }
-
 }
