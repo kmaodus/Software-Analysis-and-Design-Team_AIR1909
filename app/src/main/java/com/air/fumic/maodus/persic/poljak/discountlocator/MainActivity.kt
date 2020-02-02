eb3a70f0fa572665f4991da4e32092679f1056a1
Patrik Fumić <patrik.fumic@gmail.com>
2020-02-02 14:45:58 +0100

    StoreRecyclerView prepared for displaying data
    activity_main.xml now displays only recycler view,
    fully implemented StoreRecyclerAdapter,
    added function into MainActivity.kt that initializes RecyclerView

--- app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
+++ app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.air.fumic.maodus.persic.poljak.discountlocator
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.air.fumic.maodus.persic.poljak.database.DAO
 import com.air.fumic.maodus.persic.poljak.database.MyDb
 import com.air.fumic.maodus.persic.poljak.discountlocator.loaders.DbDataLoader
+import com.air.fumic.maodus.persic.poljak.webservice.MyWebserviceCaller
 import com.example.core.DataLoader
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 private val TAG = "MainActivity"
@@ -19,32 +16,29 @@ private val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     public var database: MyDb = MyDb().getInstance()
 
+    private lateinit var storeAdapter: StoreRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val dataLoader : DataLoader = DbDataLoader()
-
         val database : DAO = MyDb()
+        val myWebservice = MyWebserviceCaller()
 
-        val testButton: Button = findViewById(R.id.show_discounts_button)
-        val listView: ListView = findViewById(R.id.discount_list)
+        initializeRecyclerView()
+        addDataSetForRecyclerView()
+    }
 
-        val currentContext: Context = this
+    private fun addDataSetForRecyclerView(){
 
-        testButton.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                val listOfStrings = mutableListOf<String>("first", "second")
+    }
 
-                val adapter = ArrayAdapter(currentContext, android.R.layout.simple_list_item_1, listOfStrings)
-                listView.adapter = adapter
-
-                Log.d(TAG, "Successful click")
-            }
-        })
-
-
+    private fun initializeRecyclerView(){
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            storeAdapter = StoreRecyclerAdapter()
+            recycler_view.adapter = storeAdapter
+        }
     }
 }
--- app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/StoreRecyclerAdapter.kt
+++ app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/StoreRecyclerAdapter.kt
@@ -1,4 +1,63 @@
 package com.air.fumic.maodus.persic.poljak.discountlocator
 
-class StoreRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.core.DataClasses.Store
+import kotlinx.android.synthetic.main.layout_recycler_list_item.view.*
+
+class StoreRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var items: List<Store> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+       return StoreViewHolder(
+           LayoutInflater.from(parent.context).inflate(R.layout.layout_recycler_list_item, parent, false)
+       )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is StoreViewHolder ->{
+                holder.bind(items.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(storeList: List<Store>){
+        items = storeList
+    }
+
+
+    class StoreViewHolder constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+        //itemView.ID se odnosi na layout_recycler_list_item
+        val storeImage: ImageView = itemView.store_image
+        val storeName: TextView = itemView.store_name
+        val storeDescription: TextView = itemView.store_description
+
+        fun bind(store: Store){
+            storeName.setText(store.name)
+            storeDescription.setText(store.description)
+
+            val requestOptions = RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+
+            Glide.with(itemView.context)
+                .applyDefaultRequestOptions(requestOptions)
+                .load(store.imageUrl)
+                .into(storeImage)
+        }
+    }
+
 }
